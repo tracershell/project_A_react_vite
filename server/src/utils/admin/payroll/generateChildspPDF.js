9c53e2c2c1c6b8db968c8005fd96eb819898a0a2
tracershell <apple2ne1@me.com>
2025-06-15 09:25:48 -0700

    Child Support PDF adjust

--- server/src/utils/admin/payroll/generateChildspPDF.js
+++ server/src/utils/admin/payroll/generateChildspPDF.js
@@ -27,12 +27,23 @@ const generateChildspPDF = (res, query) => {
   doc.addPage();
 
   // ✅ .jpg 또는 .png만 image 삽입 가능
-  doc.image(imagePath, 0, 0, { width: doc.page.width });
+  doc.image(imagePath, 50, 100, { width: doc.page.width - 100, height: 220 });
 
   // ✅ 텍스트 삽입
-  doc.fontSize(10);
-  doc.text(`$${cs_amount}`, 450, 200);
-  doc.text(`Date: ${cs_date}    ${cs_checkno}   ${cs_name}`, 60, 400);
+  doc.fontSize(15);
+  doc.text(`${cs_amount}`, 420, 185);
+
+  // ✅ 텍스트 삽입 전: 가로선 추가
+  doc.moveTo(50, 650).lineTo(doc.page.width - 50, 650).stroke();
+
+  doc.fontSize(12);
+  doc.text(`Date: ${cs_date}`, 60, 680);
+  doc.text(`Check No.  ${cs_checkno}`, 200, 680);
+  doc.text(`$${cs_amount}`, 340, 680);
+  doc.text(`( Name: ${cs_name} )`, 420, 680);
+
+  // ✅ 텍스트 삽입 후: 가로선 추가
+  doc.moveTo(50, 720).lineTo(doc.page.width - 50, 720).stroke();
 
   doc.end();
 };
